5505ab28e53e84e8b03b9efba192c6f714f9f71f
victorqrz <victoorqrz@gmail.com>
2021-06-07 21:28:35 -0300

    creating routes

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { View, Text} from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './src/routes';
 
 export default function App(){
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Hello World</Text>
-    </View>
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
   )
 }
